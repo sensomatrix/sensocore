cc0ca8bd4d958432d37e2cdb2e8d4212e3063495
Niroigen Suntharam <niroigen@gmail.com>
2019-01-29 22:14:34 -0500

    Cleaning up code

--- widgets/ecg_parameters.py
+++ widgets/ecg_parameters.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGroupBox, QDockWidget, QHBoxLayout, QSlider
+from PyQt5.QtWidgets import QGroupBox, QDockWidget, QHBoxLayout, QSlider, QLabel
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QGridLayout
 
@@ -16,45 +16,36 @@ class ECGSimulationParameters(QDockWidget):
 			self.initLayout()
 
 		def initP(self):
-			self.p_group = QGroupBox(self)
+			self.p_group = QGroupBox('P Wave Parameters',self)
 
-			self.p_w_slider = QSlider(Qt.Horizontal)
-			self.p_m_slider = QSlider(Qt.Horizontal)
-			self.p_t_slider = QSlider(Qt.Horizontal)
+			self.p_slider = [QSlider(Qt.Horizontal) for _ in range(3)]
+			p_label = [QLabel('Widthness'), QLabel('Magnitude'), QLabel('Time')]
 
-			layout = QHBoxLayout()
+			layout = QGridLayout()
 
-			layout.addWidget(self.p_w_slider)
-			layout.addWidget(self.p_m_slider)
-			layout.addWidget(self.p_t_slider)
+			for i in range(3):
+				layout.addWidget(p_label[i],0,i)
+				layout.addWidget(self.p_slider[i],1,i)
 
 			self.p_group.setLayout(layout)
 
 		def initQ(self):
-			self.q_group = QGroupBox(self)
-			
-			self.q_1_w_slider = QSlider(Qt.Horizontal)
-			self.q_1_m_slider = QSlider(Qt.Horizontal)
-			self.q_1_t_slider = QSlider(Qt.Horizontal)
+			self.q_group = QGroupBox('Q Wave Parameters',self)
 
-			self.q_2_w_slider = QSlider(Qt.Horizontal)
-			self.q_2_m_slider = QSlider(Qt.Horizontal)
-			self.q_2_t_slider = QSlider(Qt.Horizontal)
+			self.q_slider = [QSlider(Qt.Horizontal) for _ in range(6)]
+			q_label = [QLabel('Q1 Widthness'), QLabel('Q1 Magnitude'), QLabel('Q1 Time'),
+					   QLabel('Q2 Widthness'), QLabel('Q2 Magnitude'), QLabel('Q2 Time')]
 
-			layout = QHBoxLayout()
+			layout = QGridLayout()
 
-			layout.addWidget(self.q_1_w_slider)
-			layout.addWidget(self.q_1_m_slider)
-			layout.addWidget(self.q_1_t_slider)
-
-			layout.addWidget(self.q_2_w_slider)
-			layout.addWidget(self.q_2_m_slider)
-			layout.addWidget(self.q_2_t_slider)
-
+			for i in range(6):
+				layout.addWidget(q_label[i],0,i)
+				layout.addWidget(self.q_slider[i],1,i)
+				
 			self.q_group.setLayout(layout)
 
 		def initR(self):
-			self.r_group = QGroupBox(self)
+			self.r_group = QGroupBox('R Wave Parameters', self)
 
 			self.r_w_slider = QSlider(Qt.Horizontal)
 			self.r_m_slider = QSlider(Qt.Horizontal)
@@ -69,7 +60,7 @@ class ECGSimulationParameters(QDockWidget):
 			self.r_group.setLayout(layout)
 
 		def initS(self):
-			self.s_group = QGroupBox(self)
+			self.s_group = QGroupBox('S Wave Parameters', self)
 
 			self.s_w_slider = QSlider(Qt.Horizontal)
 			self.s_m_slider = QSlider(Qt.Horizontal)
@@ -84,7 +75,7 @@ class ECGSimulationParameters(QDockWidget):
 			self.s_group.setLayout(layout)
 
 		def initT(self):
-			self.t_group = QGroupBox(self)
+			self.t_group = QGroupBox('T Wave Parameters', self)
 
 			self.t_w_slider = QSlider(Qt.Horizontal)
 			self.t_m_slider = QSlider(Qt.Horizontal)
