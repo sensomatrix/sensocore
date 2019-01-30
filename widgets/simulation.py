36860d3f7f87b155ce0f23044022542762a8d6cc
Niroigen Suntharam <niroigen@gmail.com>
2019-01-29 19:30:48 -0500

    Creating basic graph for simulation

---
+++ widgets/simulation.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import pyqtgraph as pg
+ 
+class Simulation(QMainWindow):
+	def __init__(self, title='ECG Simulation'):
+		super().__init__()
+		self.title = title
+		self.initUI()
+	 
+	def initUI(self):
+		self.setWindowTitle(self.title)
+		self.sim_graph = SimulationGraph()
+		self.setCentralWidget(self.sim_graph)
+		self.show()
+
+class SimulationGraph(pg.PlotWidget):
+	def __init__(self):
+		super().__init__()
+		self.plotItem = self.getPlotItem()
+		self.plotItem.showAxis('right')
+		self.plotItem.hideAxis('left')
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = Simulation()
+	sys.exit(app.exec_())
