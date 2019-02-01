847ae025752d8c4b45846c73cab55ee390a1b624
Taran <taran.rainaa@gmail.com>
2019-01-31 22:23:23 -0500

    Adding graph to dialog

--- widgets/simulation.py
+++ widgets/simulation.py
@@ -1,13 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QHBoxLayout
+import pyqtgraph as pg
 
 class Simulation(QDialog):
 	def __init__(self, title):
-		self.title = title
+		self._title = title
 		super().__init__()
+		self.initUI()
 
 	def initUI(self):
-		self.setWindowTitle(self.title)
+		self.setWindowTitle(self._title)
+		self._sim_graph = pg.PlotWidget()
+		self._sim_graph.setBackground('w')
+		_h_box = QHBoxLayout()
+		_h_box.addWidget(self._sim_graph)
+		self.setLayout(_h_box)
 
 if __name__ == "__main__":
 	global app
