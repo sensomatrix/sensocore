c3617c2b98a406d8476e1befa4f2528f9ea2cd2c
Taran <taran.rainaa@gmail.com>
2019-02-04 23:21:18 -0500

    Removing the print message

--- widgets/channels.py
+++ widgets/channels.py
@@ -40,7 +40,6 @@ class Channels(QWidget):
 
     def create_menu(self, position):
         if len(self.channel_list.selectedItems()) != 0:
-            print(self.channel_list.selectedItems())
             item = self.channel_list.selectedItems()[0]
             id = item.data(Qt.UserRole)
             sig = self.signal_dict.get(id)
