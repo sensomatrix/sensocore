2a90c6fca51bc5f68329f73ef83f2b45e613a6ed
abuebola <andre.kh@protonmail.com>
2019-02-06 11:00:34 -0500

    time axis injection fix

--- widgets/timeaxisitem.py
+++ widgets/timeaxisitem.py
@@ -8,13 +8,9 @@ class TimeAxisItem(pg.AxisItem):
         pg.AxisItem.__init__(self, *args, **kwargs)
 
     def tickStrings(self, values, scale, spacing):
-        places = max(0, np.ceil(-np.log10(spacing * scale)))
         strings = []
         for v in values:
             vs = v * scale
-            if abs(vs) < .001 or abs(vs) >= 10000:
-                vstr = secondsToHHMMSSMMM(vs)
-            else:
-                vstr = secondsToHHMMSSMMM(vs)
+            vstr = secondsToHHMMSSMMM(vs)
             strings.append(vstr)
         return strings
