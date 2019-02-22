b79494f69e0ab51fc3c231680d3271e0d8ec249f
Niroigen Suntharam <niroigen@gmail.com>
2019-02-22 01:02:11 -0500

    Adding a crosshair for the ECG simulation

--- widgets/ecg_sim_widget.py
+++ widgets/ecg_sim_widget.py
@@ -19,7 +19,27 @@ class MainWindow(TemplateBaseClass):
         self.ui = ECGSimulation()
         self.ui.setupUi(self)
 
-        # Update plot
+        # cross hair
+        self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        self.ui.plot.addItem(self.vLine, ignoreBounds=True)
+        self.ui.plot.addItem(self.hLine, ignoreBounds=True)
+        self.proxy = pg.SignalProxy(self.ui.plot.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+# Saving default values
+###############################################################################################
+        self.p_default = self.p
+        self.q_default = self.q
+        self.r_default = self.r
+        self.s_default = self.s
+        self.t_default = self.t
+        self.fs_default = self.sampling_frequency
+        self.delay_default = self.delay
+        self.noise_default = self.noise
+        self.period_default = self.period
+###############################################################################################
+
+# Connections
 ###############################################################################################
         # P values being updated
         self.ui.p_mag_double_spinbox.valueChanged.connect(self.update_plot)
@@ -56,15 +76,61 @@ class MainWindow(TemplateBaseClass):
         self.ui.sampling_frequency_spinbox.valueChanged.connect(self.update_plot)
         self.ui.delay_spin_box.valueChanged.connect(self.update_plot)
 
+        self.ui.reset_signal_button.clicked.connect(self.reset_to_default)
 ###############################################################################################
+
+        time, ecg_output = generate_ecg(self.sampling_frequency, self.noise, self.duration, self.period, self.delay,
+                           self.p, self.q, self.r, self.s, self.t)
+
+        self.plot = self.ui.plot.plot(time, ecg_output)
 
         self.show()
 
+# Methods
+###############################################################################################
     def update_plot(self):
-        self.ui.plot.clear()
         time, ecg_output = generate_ecg(self.sampling_frequency, self.noise, self.duration, self.period, self.delay,
                            self.p, self.q, self.r, self.s, self.t)
-        self.ui.plot.plot(time, ecg_output)
+
+        self.plot.setData(time, ecg_output)
+
+    def reset_to_default(self):
+        self.ui.p_mag_double_spinbox.setValue(self.p_default[0])
+        self.ui.p_time_double_spinbox.setValue(self.p_default[1])
+        self.ui.p_wid_double_spinbox.setValue(self.p_default[2])
+
+        self.ui.q1_mag_double_spinbox.setValue(self.q_default[0])
+        self.ui.q1_time_double_spinbox.setValue(self.q_default[1])
+        self.ui.q1_wid_double_spinbox.setValue(self.q_default[2])
+
+        self.ui.q2_mag_double_spinbox.setValue(self.q_default[0])
+        self.ui.q2_time_double_spinbox.setValue(self.q_default[1])
+        self.ui.q2_wid_double_spinbox.setValue(self.q_default[2])
+
+        self.ui.r_mag_double_spinbox.setValue(self.r_default[0])
+        self.ui.r_time_double_spinbox.setValue(self.r_default[1])
+        self.ui.r_wid_double_spinbox.setValue(self.r_default[2])
+
+        self.ui.s_mag_double_spinbox.setValue(self.s_default[0])
+        self.ui.s_time_double_spinbox.setValue(self.s_default[1])
+        self.ui.s_wid_double_spinbox.setValue(self.s_default[2])
+
+        self.ui.t_mag_double_spinbox.setValue(self.t_default[0])
+        self.ui.t_time_double_spinbox.setValue(self.t_default[1])
+        self.ui.t_wid_double_spinbox.setValue(self.t_default[2])
+
+        self.ui.sampling_frequency_spinbox.setValue(self.fs_default)
+        self.ui.delay_spin_box.setValue(self.delay_default)
+        self.ui.period_spinbox.setValue(self.period_default)
+        self.ui.noise_double_spinbox.setValue(self.noise_default)
+
+    def mouseMoved(self, evt):
+        pos = evt[0]  ## using signal proxy turns original arguments into a tuple
+        if self.ui.plot.sceneBoundingRect().contains(pos):
+            mousePoint = self.ui.plot.plotItem.vb.mapSceneToView(pos)
+            self.vLine.setPos(mousePoint.x())
+            self.hLine.setPos(mousePoint.y())
+###############################################################################################
 
     # Properties
 ###############################################################################################
