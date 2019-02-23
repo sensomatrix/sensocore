383e5f47ddb14c989da6032824c2e2ebb2425759
Niroigen Suntharam <niroigen@gmail.com>
2019-02-23 15:29:59 -0500

    Removing commented code

--- widgets/main_window.py
+++ widgets/main_window.py
@@ -20,8 +20,6 @@ class MainWindow(TemplateBaseClass):
         self.ui.setupUi(self)
 
         self.ui.main_tab.setEnabled(True)
-        # self.ui.oscilloscope_tab.display_graph([1,2,3,4,5,6,7,8,9,10], np.transpose([-10,0,4,1,2,7,5,3,8,0]))
-        # self.ui.oscilloscope_tab.display_graph([0,1,2,3,4,5,6,7,8,9,20], np.transpose([1,5,3,8,9,24,2,5,12,10,33]))
 
         self.ui.actionEEG_Simulation.triggered.connect(self.launch_eeg_widget)
         self.ui.actionECG_Simulation.triggered.connect(self.launch_ecg_widget)
