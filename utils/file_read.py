449dd02469f8c74230e3591e4a5a237118c39b64
Ajevan Nadarajah <ajevan3@hotmail.com>
2019-02-27 19:53:17 -0500

    added load_from_json method to file_read.py

--- utils/file_read.py
+++ utils/file_read.py
@@ -5,6 +5,7 @@ import numpy as np
 from PyQt5.QtWidgets import QAction, QFileDialog
 from PyQt5.QtCore import pyqtSignal, QObject
 from pathlib import Path
+from jsonparser import JsonParser
 
 # dialog used to load a complete dataset. to do: deal with the case user clicks on "cancel"
 def open_dataset_dialog(parent):
@@ -59,4 +60,31 @@ def readnextlines(fileObject, n):
     #     self.signals_dictionary.get(id_).samples_array = new_samples_array
     #     self.signal_changed_signal.emit(id_)
 
-
+def load_from_json(self, filepath):
+    signals = []
+    data = JsonParser(filepath)
+    for c in data.channels.values():
+        # IBI interbeat interval skip for now
+        if c.name == "IBI":
+            continue
+        # if multiple signals in 1 channel
+        if isinstance(c.fs, list):
+            for s in range(len(c.fs)):
+                name = c.name
+                typeofsignal = c.sensor
+                samplingrate = c.fs[s]
+                time_array = (np.asarray(c.time_array[s])).astype(np.float32)
+                samples_array = (np.asarray(c.samples_array[s])).astype(np.float32)
+                sig = Signal(samples_array, time_array=time_array, name=name, type=typeofsignal)
+                signals.append(sig)
+               # self.signals_dictionary[sig.id] = sig
+        else:  # if single signal in 1 channel
+            name = c.name
+            typeofsignal = c.sensor
+            samplingrate = c.fs
+            time_array = (np.asarray(c.time_array)).astype(np.float32)
+            samples_array = (np.asarray(c.samples_array)).astype(np.float32)
+            sig = Signal(samples_array, time_array=time_array, name=name, type=typeofsignal)
+            signals.append(sig)
+            #self.signals_dictionary[sig.id] = sig
+    return signals
