81dd4801ac66c8e1fed4cba3e678225921039071
Niroigen Suntharam <niroigen@gmail.com>
2019-03-11 18:18:35 -0400

    Overlay PSD on top of other PSDs

--- widgets/graphdock.py
+++ widgets/graphdock.py
@@ -3,20 +3,24 @@ import numpy as np
 from pyqtgraph.dockarea import *
 from utils.frequtils import compute_psd, compute_time_freq
 
+LINECOLORS = ['r', 'g', 'b', 'c', 'm', 'y', 'w']
 
 class GraphDock(Dock):
 
     def __init__(self, parent, name):
         super().__init__(name, size=(1, 1), closable=True)
         self.parent = parent
-        pg.setConfigOptions(imageAxisOrder='row-major')
+        # pg.setConfigOptions(imageAxisOrder='row-major')
         self.pgview = pg.GraphicsView()
         self.graphLayout = pg.GraphicsLayout()
         self.pgview.setCentralItem(self.graphLayout)
+        self.p = pg.PlotItem()
+        self.graphLayout.addItem(self.p)
         self.addWidget(self.pgview)
+        self.current_index = 0
 
     def plot(self, signal, title="", XAxisLabel="", YAxisLabel="", XAxisUnits="", YAxisUnits=""):
-        self.graphLayout.nextRow()
+        # self.graphLayout.nextRow()
         XAxis = pg.AxisItem(orientation='bottom')
         XAxis.setLabel(text=XAxisLabel, units=XAxisUnits)
         YAxis = pg.AxisItem(orientation='left')
@@ -25,11 +29,12 @@ class GraphDock(Dock):
             'bottom': XAxis,
             'left': YAxis
         }
-        p = pg.PlotItem(title=title, axisItems=axisdict)
-        PSDfbins, PSDxx = compute_psd(signal.samples_array, signal.fs)
-        p.plot(PSDfbins, PSDxx)
-        self.graphLayout.addItem(p)
-        p.vb.autoRange()
+        # p = pg.PlotItem(title=title, axisItems=axisdict)
+        PSDfbins, PSDxx = compute_psd(signal.current_mode, signal.fs)
+        self.p.plot(PSDfbins, PSDxx, pen=pg.mkPen(LINECOLORS[self.current_index]))
+        self.current_index = (self.current_index + 1) % len(LINECOLORS)
+        self.p.vb.autoRange()
+        self.p.replot()
 
     # Following code is from: https://stackoverflow.com/questions/51312923/plotting-the-spectrum
     def plot_TF(self, signal):
