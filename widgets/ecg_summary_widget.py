b32872b57c12e1df5b47507433acba462fe1d394
Niroigen Suntharam <niroigen@gmail.com>
2019-03-11 19:44:20 -0400

    View ECG Summary

--- widgets/ecg_summary_widget.py
+++ widgets/ecg_summary_widget.py
@@ -16,14 +16,21 @@ class ECGSummaryWidget(TemplateBaseClass):
         self.ui = ECGSummaryView()
         self.ui.setupUi(self)
 
-        # (ts, filtered, rpeaks, ts_tmpl, templates, ts_hr, hr)
+        self.ui.raw.plot(ecg[0], raw, pen='r')
+        self.ui.raw.setLabel('left', "Amplitude", units='A')
+        self.ui.raw.setLabel('bottom', "Time", units='s')
 
-        self.ui.raw.plot(ecg[0], raw)
-        self.ui.filtered.plot(ecg[0], ecg[1])
-        self.ui.hrv.plot(ecg[5], ecg[6])
-        T = ecg[4].T
-        template = np.zeros(T.shape[0])
-        for index, row in enumerate(T):
+        self.ui.filtered.plot(ecg[0], ecg[1], pen='g')
+        self.ui.filtered.setLabel('left', "Amplitude", units='A')
+        self.ui.filtered.setLabel('bottom', "Time", units='s')
+
+        self.ui.hrv.plot(ecg[5], ecg[6], pen='y')
+        self.ui.hrv.setLabel('left', "Heart Rate")
+        self.ui.hrv.setLabel('bottom', "Time", units='s')
+
+        t = ecg[4].T
+        template = np.zeros(t.shape[0])
+        for index, row in enumerate(t):
             template[index] = row[0]
-        self.ui.template_plot.plot(ecg[3], template)
+        self.ui.template_plot.plot(ecg[3], template, pen='w')
         self.show()
