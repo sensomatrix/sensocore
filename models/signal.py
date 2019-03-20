0d269550582b7268e0128f768be84e2e3b48fa7e
Niroigen Suntharam <niroigen@gmail.com>
2019-03-20 04:17:58 -0400

    Using models to find abnormality detection for ecg signal

--- models/signal.py
+++ models/signal.py
@@ -1,10 +1,27 @@
+from __future__ import division, print_function
 from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSignal
 from numpy import mean
 import copy
 from biosppy import signals, clustering
 import numpy as np
-#TODO: Remove this after being used
+from utils.frequtils import compute_psd
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+import biosppy
+import json
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 
 
 class Signal:
@@ -13,6 +30,7 @@ class Signal:
         self.raw = samples_array.T[1]
         self.filtered = None
         self.clusters = None
+        self.psd = None
         self.time_array = samples_array.T[0]
         self.fs = fs
         self.name = name
@@ -24,10 +42,114 @@ class Signal:
         if 'EEG' in self.type:
             eeg = np.transpose(self.current_mode).reshape((self.current_mode.shape[0], 1))
             self.summary = signals.eeg.eeg(eeg, self.fs, show=False)
+            diff = int(eeg.shape[0] % (self.fs / 2))
+            if diff != 0:
+                eeg = np.delete(eeg, eeg.shape[0] - 1)
+            rows = eeg.shape[0] // (self.fs // 2)
+            eeg = eeg.reshape((rows, self.fs // 2))
+
+            eeg_psd = np.array([compute_psd(eeg[0][:], self.fs)[1]])
+
+            for row in range(1, rows):
+                eeg_psd = np.vstack([eeg_psd, compute_psd(eeg[row][:], self.fs)[1]])
+
+            self.clusters = clustering.dbscan(eeg_psd)
+            print('hi')
         elif 'ECG' in self.type:
             ecg = np.transpose(self.current_mode)
             self.summary = signals.ecg.ecg(ecg, self.fs, show=False)
             self.clusters = clustering.dbscan(self.summary[4])
+            self.model_predict(self.generate_csv())
+
+    def generate_csv(self):
+        a = np.asarray(self.current_mode)
+        np.savetxt("{0}.csv".format(self.name), a, delimiter=",",  header=' Sample Value', comments='')
+        return os.getcwd() + '/' + self.name + '.csv'
+
+    def model_predict(self, path):
+        model = load_model('/home/niroigen/Downloads/ecgScratchEpoch2.hdf5')
+        model._make_predict_function()  # Necessary
+        output = []
+
+        flag = 1
+
+        print('what is it')
+        # index1 = str(path).find('sig-2') + 6
+        # index2 = -4
+        # ts = int(str(path)[index1:index2])
+        APC, NORMAL, LBB, PVC, PAB, RBB, VEB = [], [], [], [], [], [], []
+        output.append(str(path))
+        result = {"APC": APC, "Normal": NORMAL, "LBB": LBB, "PAB": PAB, "PVC": PVC, "RBB": RBB, "VEB": VEB}
+
+        indices = []
+
+        kernel = np.ones((4, 4), np.uint8)
+
+        csv = pd.read_csv(path)
+        csv_data = csv[' Sample Value']
+        data = np.array(csv_data)
+        signals = []
+        count = 1
+        peaks = biosppy.signals.ecg.christov_segmenter(signal=data, sampling_rate=self.fs)[0]
+        for i in (peaks[1:-1]):
+            diff1 = abs(peaks[count - 1] - i)
+            diff2 = abs(peaks[count + 1] - i)
+            x = peaks[count - 1] + diff1 // 2
+            y = peaks[count + 1] - diff2 // 2
+            signal = data[x:y]
+            signals.append(signal)
+            count += 1
+            indices.append((x, y))
+
+        for count, i in enumerate(signals):
+            fig = plt.figure(frameon=False)
+            plt.plot(i)
+            plt.xticks([]), plt.yticks([])
+            for spine in plt.gca().spines.values():
+                spine.set_visible(False)
+
+            filename = 'fig' + '.png'
+            fig.savefig(filename)
+            im_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+            im_gray = cv2.erode(im_gray, kernel, iterations=1)
+            im_gray = cv2.resize(im_gray, (128, 128), interpolation=cv2.INTER_LANCZOS4)
+            cv2.imwrite(filename, im_gray)
+            im_gray = cv2.imread(filename)
+            pred = model.predict(im_gray.reshape((1, 128, 128, 3)))
+            pred_class = pred.argmax(axis=-1)
+            if pred_class == 0:
+                APC.append(indices[count])
+            elif pred_class == 1:
+                NORMAL.append(indices[count])
+            elif pred_class == 2:
+                LBB.append(indices[count])
+            elif pred_class == 3:
+                PAB.append(indices[count])
+            elif pred_class == 4:
+                PVC.append(indices[count])
+            elif pred_class == 5:
+                RBB.append(indices[count])
+            elif pred_class == 6:
+                VEB.append(indices[count])
+
+        result = sorted(result.items(), key=lambda y: len(y[1]))[::-1]
+        output.append(result)
+        data = {}
+        data['filename' + str(flag)] = str(path)
+        data['result' + str(flag)] = str(result)
+
+        # json_filename = 'data.txt'
+        # with open(json_filename, 'a+') as outfile:
+        #     json.dump(data, outfile)
+        # flag += 1
+        #
+        # with open(json_filename, 'r') as file:
+        #     filedata = file.read()
+        # filedata = filedata.replace('}{', ',')
+        # with open(json_filename, 'w') as file:
+        #     file.write(filedata)
+        # os.remove('fig.png')
+        return output
 
     def remove_dc(self):
         self.current_mode = self.current_mode - mean(self.current_mode)
