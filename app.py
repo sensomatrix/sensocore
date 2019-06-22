de8656428b8db8351a364a23c6c5cb1b9aa7f678
Niroigen Suntharam <niroigen@gmail.com>
2019-06-22 19:04:38 -0400

    Adding an error handler

--- app.py
+++ app.py
@@ -37,5 +37,11 @@ def get_signal(signal_id):
         abort(404)
     return jsonify({'signal': signal[0]})
 
+from flask import make_response
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
