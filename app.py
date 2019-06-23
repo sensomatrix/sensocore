e85409981b57d5c788a7117953d100ae0529d605
Niroigen Suntharam <niroigen@gmail.com>
2019-06-23 18:23:06 -0400

    Able to create a signal

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -11,9 +11,30 @@ db = SQLAlchemy(app)
 from models import Signal
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+            unit = request.form['unit']
+            fs = request.form['fs']
+            try:
+                signal = Signal(
+                    name=name,
+                    unit=unit,
+                    fs=fs
+                )
+                db.session.add(signal)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+        except:
+            errors.append(
+                "Unable to create signal"
+            )
+    return render_template('index.html', errors=errors)
 
 
 if __name__ == '__main__':
