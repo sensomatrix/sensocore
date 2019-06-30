8f155828ec0fe86e9feff66035d17d6285b6a44c
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 18:06:14 -0400

    Getting migrations

--- manage.py
+++ manage.py
@@ -2,16 +2,18 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
+from src.models.UserModel import UserModel
+from src.models.SignalModel import SignalModel
+from src.app import create_app, db
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+migrate = Migrate(app=app, db=db, user=UserModel, signal=SignalModel)
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+manager = Manager(app=app)
 
 manager.add_command('db', MigrateCommand)
 
-
 if __name__ == '__main__':
-    manager.run()
+  manager.run()
--- src/config.py
+++ src/config.py
@@ -10,6 +10,7 @@ class Development(object):
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class Production(object):
     """
@@ -19,6 +20,7 @@ class Production(object):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app_config = {
     'development': Development,
