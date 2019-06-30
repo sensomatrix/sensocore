4baa7d1a672ef5b8b95febb0b60a5ba527b27cf8
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 17:13:48 -0400

    Creating user model

---
+++ src/models/UserModel.py
@@ -0,0 +1,56 @@
+# src/models/UserModel.py
+from marshmallow import fields, Schema
+import datetime
+from . import db
+
+class UserModel(db.Model):
+  """
+  User Model
+  """
+
+  # table name
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(128), nullable=False)
+  email = db.Column(db.String(128), unique=True, nullable=False)
+  password = db.Column(db.String(128), nullable=True)
+  created_at = db.Column(db.DateTime)
+  modified_at = db.Column(db.DateTime)
+
+  # class constructor
+  def __init__(self, data):
+    """
+    Class constructor
+    """
+    self.name = data.get('name')
+    self.email = data.get('email')
+    self.password = data.get('password')
+    self.created_at = datetime.datetime.utcnow()
+    self.modified_at = datetime.datetime.utcnow()
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self, data):
+    for key, item in data.items():
+      setattr(self, key, item)
+    self.modified_at = datetime.datetime.utcnow()
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  @staticmethod
+  def get_all_users():
+    return UserModel.query.all()
+
+  @staticmethod
+  def get_one_user(id):
+    return UserModel.query.get(id)
+
+  
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
