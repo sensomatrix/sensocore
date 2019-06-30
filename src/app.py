e08006985f8b7ddb6ae12ab3ddbe4b9a701e121d
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 18:16:57 -0400

    Gets all the endpoints for user

--- src/app.py
+++ src/app.py
@@ -5,6 +5,8 @@ from flask import Flask
 from .config import app_config
 from .models import db, bcrypt
 
+from .views.UserView import user_api as user_blueprint
+
 def create_app(env_name):
   """
   Create app
@@ -18,6 +20,8 @@ def create_app(env_name):
   bcrypt.init_app(app)
 
   db.init_app(app)
+
+  app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
 
   @app.route('/', methods=['GET'])
   def index():
