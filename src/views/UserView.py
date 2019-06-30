c2c7f9d93eec1e713345b803bf9324127d6a6e62
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 18:15:43 -0400

    Getting the rest of the CRUD for user

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -64,6 +64,55 @@ def login():
 
   return custom_response({'jwt_token': token}, 200)
 
+@user_api.route('/<int:user_id>', methods=['GET'])
+@Auth.auth_required
+def get_a_user(user_id):
+  """
+  Get a single user
+  """
+  user = UserModel.get_one_user(user_id)
+  if not user:
+    return custom_response({'error': 'user not found'}, 404)
+  
+  ser_user = user_schema.dump(user).data
+  return custom_response(ser_user, 200)
+
+@user_api.route('/me', methods=['PUT'])
+@Auth.auth_required
+def update():
+  """
+  Update me
+  """
+  req_data = request.get_json()
+  data, error = user_schema.load(req_data, partial=True)
+  if error:
+    return custom_response(error, 400)
+
+  user = UserModel.get_one_user(g.user.get('id'))
+  user.update(data)
+  ser_user = user_schema.dump(user).data
+  return custom_response(ser_user, 200)
+
+@user_api.route('/me', methods=['DELETE'])
+@Auth.auth_required
+def delete():
+  """
+  Delete a user
+  """
+  user = UserModel.get_one_user(g.user.get('id'))
+  user.delete()
+  return custom_response({'message': 'deleted'}, 204)
+
+@user_api.route('/me', methods=['GET'])
+@Auth.auth_required
+def get_me():
+  """
+  Get me
+  """
+  user = UserModel.get_one_user(g.user.get('id'))
+  ser_user = user_schema.dump(user).data
+  return custom_response(ser_user, 200)
+
 def custom_response(res, status_code):
   """
   Custom Response Function
