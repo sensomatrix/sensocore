ed6c2771e2836e10f16a6c5f59c30c32b8218777
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 17:01:20 -0400

    Recreating flask blog post api tutorial

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
-from app import db
+from app import db, bcrypt
 from sqlalchemy.dialects.postgresql import JSON
-
+from marshmallow import fields, Schema
 
 class Signal(db.Model):
     __tablename__ = 'signals'
 
     id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForgeignKey('users.id'), nullable=False)
     name = db.Column(db.String())
     unit = db.Column(db.String())
     fs = db.Column(db.Integer())
@@ -14,6 +15,107 @@ class Signal(db.Model):
         self.name = name
         self.unit = unit
         self.fs = fs
+        self.owner_id = data.get('owner_id')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+          setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class SignalSchema(Schema):
+  """
+  Signal Schema
+  """
+  id = fields.Int(dump_only=True)
+  name = fields.Str(required=True)
+  unit = fields.Str(required=True)
+  owner_id = fields.Int(required=True)
+  fs = fields.Int(dump_only=True)
+
+
+class UserModel(db.Model):
+    """
+    User Model
+    """
+
+    # table name
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=True)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+    signals = db.relationship('Signal', backref='signals', lazy=True)
+
+    # class constructor
+    def __init__(self, data):
+        """
+        Class constructor
+        """
+        self.name = data.get('name')
+        self.email = data.get('email')
+        self.password = self.__generate_has(data.get('password'))
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            if key == 'password': # add this new line
+                self.password = self.__generate_hash(value) # add this new line
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow()
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __generate_hash(self, password):
+        return bcrypt.generate_password_hash(password, rounds=10).decode("utf-8")
+
+    def check_hash(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
+    @staticmethod
+    def get_all_users():
+        return UserModel.query.all()
+
+    @staticmethod
+    def get_one_user(id):
+        return UserModel.query.get(id)
+
+  
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class UserSchema(Schema):
+    """
+    User Schema
+    """
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
+    signals = fields.Nested(SignalSchema, many=True)
--- src/app.py
+++ src/app.py
@@ -1,15 +1,16 @@
 import os
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from models import Signal
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
