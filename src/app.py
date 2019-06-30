64d882c34b3910f52e448bd09945260a837e1cb9
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 17:58:01 -0400

    wrapping db and bcrypt to app

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
 
 def create_app(env_name):
   """
@@ -13,6 +14,10 @@ def create_app(env_name):
   app = Flask(__name__)
 
   app.config.from_object(app_config[env_name])
+
+  bcrypt.init_app(app)
+
+  db.init_app(app)
 
   @app.route('/', methods=['GET'])
   def index():
