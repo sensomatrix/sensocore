1890b63f64ca1230df950787fba38955ce14eac5
Niroigen Suntharam <niroigen@gmail.com>
2019-06-30 18:10:33 -0400

    Login user

--- src/views/UserView.py
+++ src/views/UserView.py
@@ -31,6 +31,31 @@ def create():
 
   return custom_response({'jwt_token': token}, 201)
   
+@user_api.route('/login', methods=['POST'])
+def login():
+  req_data = request.get_json()
+
+  data, error = user_schema.load(req_data, partial=True)
+
+  if error:
+    return custom_response(error, 400)
+  
+  if not data.get('email') or not data.get('password'):
+    return custom_response({'error': 'you need email and password to sign in'}, 400)
+  
+  user = UserModel.get_user_by_email(data.get('email'))
+
+  if not user:
+    return custom_response({'error': 'invalid credentials'}, 400)
+  
+  if not user.check_hash(data.get('password')):
+    return custom_response({'error': 'invalid credentials'}, 400)
+  
+  ser_data = user_schema.dump(user).data
+  
+  token = Auth.generate_token(ser_data.get('id'))
+
+  return custom_response({'jwt_token': token}, 200)
 
 def custom_response(res, status_code):
   """
