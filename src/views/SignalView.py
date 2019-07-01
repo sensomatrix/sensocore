32e4110d285a4ae0e37651161cefb4a68349e77f
Niroigen Suntharam <niroigen@gmail.com>
2019-07-01 01:43:13 -0400

    Able to get a signal

--- src/views/SignalView.py
+++ src/views/SignalView.py
@@ -32,6 +32,17 @@ def get_all():
   data = signal_schema.dump(signals, many=True).data
   return custom_response(data, 200)
 
+@signal_api.route('/<int:signal_id>', methods=['GET'])
+def get_one(signal_id):
+  """
+  Get A Signal
+  """
+  signal = SignalModel.get_one_signal(signal_id)
+  if not signal:
+    return custom_response({'error': 'signal not found'}, 404)
+  data = signal_schema.dump(signal).data
+  return custom_response(data, 200)
+
 def custom_response(res, status_code):
   """
   Custom Response Function
