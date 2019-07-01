5f4affd6b72cf9243118a816faf04e260747f18e
Niroigen Suntharam <niroigen@gmail.com>
2019-07-01 13:08:10 -0400

    Creating tests for user

--- src/config.py
+++ src/config.py
@@ -22,7 +22,17 @@ class Production(object):
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
 app_config = {
     'development': Development,
     'production': Production,
+    "testing": Testing
 }
