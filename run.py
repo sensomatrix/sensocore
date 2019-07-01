3e30472c022ee78098502c390070690668181483
Niroigen Suntharam <niroigen@gmail.com>
2019-07-01 13:40:57 -0400

    Adding Procfile and updating run.py

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # /run.py
 import os
-
 from src.app import create_app
 
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
 if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
+  port = os.getenv('PORT')
   # run app
-  app.run() # Can set a different port by putting app.run(port=<PORT>)
+  app.run(host='0.0.0.0', port=port)
