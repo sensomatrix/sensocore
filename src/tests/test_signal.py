de3cb011675741c3211b6e526b23ad38f6b8daa1
Niroigen Suntharam <niroigen@gmail.com>
2019-07-19 19:49:36 -0400

    Create test_simulate_ecg_less_than_one_beat

--- src/tests/test_signal.py
+++ src/tests/test_signal.py
@@ -134,12 +134,14 @@ class SignalTest(unittest.TestCase):
             '/api/v1/signals/', data=json.dumps(self.test_data), content_type='application/json')
         self.assertEqual(res.status_code, 201)
 
-        res = self.client.delete('/api/v1/signals/1', content_type='application/json')
+        res = self.client.delete(
+            '/api/v1/signals/1', content_type='application/json')
         self.assertEqual(res.status_code, 204)
 
     def test_delete_signal_nonexistant(self):
         """Test Delete a signal non existant"""
-        res = self.client.delete('/api/v1/signals/1', content_type='application/json')
+        res = self.client.delete(
+            '/api/v1/signals/1', content_type='application/json')
         self.assertEqual(res.status_code, 404)
         self.assertEqual({'error': 'signal not found'}, json.loads(res.data))
 
@@ -188,6 +190,53 @@ class SignalTest(unittest.TestCase):
         self.assertEqual(data['sensor_location_on_body'], None)
         self.assertEqual(len(data['raw']), period * fs * duration)
         self.assertEqual(data['raw'][0:fs - 1], data['raw'][fs: 2*fs - 1])
+        self.assertEqual(res.status_code, 201)
+
+    def test_simulate_ecg_less_than_one_beat(self):
+        """Test Simulate ECG that is less than its period, 
+        it should create a signal that is one beat lone"""
+        duration = 0.5
+        period = 1
+        fs = 256
+        res = self.client.post('api/v1/signals/simulate/ecg', data=json.dumps({
+            "fs": fs,
+            "noise_magnitude": 0,
+            "duration": duration,
+            "period": period,
+            "delay": 0,
+            "P": [
+                0.180,
+                0.2922,
+                0.0178
+            ],
+            "Q": [
+                -0.0223,
+                0.3218,
+                0.03064,
+                -0.2725,
+                0.37123,
+                0.00571
+            ],
+            "R": [
+                0.095,
+                0.44471,
+                0.01997
+            ],
+            "S": [
+                0.179,
+                0.46,
+                0.009
+            ],
+            "T": [
+                0.3255,
+                0.654,
+                0.02978
+            ]
+        }), content_type='application/json')
+        data = json.loads(res.data)
+        self.assertEqual(data['sensor'], None)
+        self.assertEqual(data['sensor_location_on_body'], None)
+        self.assertEqual(len(data['raw']), 256)
         self.assertEqual(res.status_code, 201)
 
     def test_simulate_ecg_error(self):
