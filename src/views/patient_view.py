47dcdd2f2120fc4c12078f39e2b0d632cb1492fa
Niroigen Suntharam <niroigen@gmail.com>
2019-07-22 19:07:00 -0400

    Patient view

---
+++ src/views/patient_view.py
@@ -0,0 +1,36 @@
+# /src/views/patient_view.py
+from flask import request, g, Blueprint, json, Response
+from src.models.signal import Signal, SignalSchema
+from src.models.device import Device, DeviceSchema
+from src.models.patient import Patient, PatientSchema
+from src.views.views_helper import create_signals
+import json
+from src.shared.file_read import json_parser
+
+patient_api = Blueprint('patient_api', __name__)
+signal_schema = SignalSchema()
+device_schema = DeviceSchema()
+patient_schema = PatientSchema()
+
+@patient_api.route('/upload', methods=['POST'])
+def upload():
+    """
+    Upload a patient
+    """
+    file = json.loads(request.files['file'].read())
+    head = list(file.keys())[0]
+    patient_info = file[head]['header']['patient_information']
+    patient_data, error = patient_schema.load(patient_info)
+    if (error):
+        return custom_response(error, 400)
+        
+
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
