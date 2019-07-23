6240b7995560f1f3309d688c5de6f5e779e90239
Niroigen Suntharam <niroigen@gmail.com>
2019-07-22 20:07:07 -0400

    Adding recording schema

--- src/views/patient_view.py
+++ src/views/patient_view.py
@@ -3,6 +3,7 @@ from flask import request, g, Blueprint, json, Response
 from src.models.signal import Signal, SignalSchema
 from src.models.device import Device, DeviceSchema
 from src.models.patient import Patient, PatientSchema
+from src.models.recording import Recording, RecordingSchema
 from src.views.views_helper import create_signals
 import json
 from src.shared.file_read import json_parser
@@ -11,6 +12,7 @@ patient_api = Blueprint('patient_api', __name__)
 signal_schema = SignalSchema()
 device_schema = DeviceSchema()
 patient_schema = PatientSchema()
+recording_schema = RecordingSchema()
 
 @patient_api.route('/upload', methods=['POST'])
 def upload():
@@ -20,10 +22,16 @@ def upload():
     file = json.loads(request.files['file'].read())
     head = list(file.keys())[0]
     patient_info = file[head]['header']['patient_information']
+    recording_info = file[head]['header']['recording_info']
     patient_data, error = patient_schema.load(patient_info)
+    
     if (error):
         return custom_response(error, 400)
-        
+    recording_data, error = recording_schema.load(recording_info)
+
+    if (error):
+        return custom_response(error, 400)
+
 
 def custom_response(res, status_code):
     """
